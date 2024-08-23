2e0f613781ca78cc308fa039448ace781e1fc79c
Isaac Denny <isaac_denny@icloud.com>
2024-08-23 10:18:53 -0400

    a1

---
+++ a1/a1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int h = 'H';
+
+    char word[6] = "Helo!3";
+
+    for (int i = 0; i < sizeof(word); i++) {
+        printf("Character: %c\n", word[i]);
+        printf("Decimal: %d\n", word[i]);
+        printf("Binary: ");
+        for (int j = 128; j > 0; j >>= 1) {
+            printf("%s", ((word[i] & j) == j) ? "1" : "0");
+        }
+
+        
+        printf("\n\n");
+    }
+
+    // 1000
+    // 0100
+    // 0010
+    // 0001
+
+    return 0;
+}
